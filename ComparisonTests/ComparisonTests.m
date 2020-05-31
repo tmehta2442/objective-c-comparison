3fc3fc07b0bc25943dc95433f80ec1577d7d4aae
TEJAS MEHTA <tejasmehta@TEJASs-MacBook-Air.local>
2020-05-31 15:57:43 -0700

    first commit

---
+++ ComparisonTests/ComparisonTests.m
@@ -0,0 +1,37 @@
+//
+//  ComparisonTests.m
+//  ComparisonTests
+//
+//  Created by TEJAS MEHTA on 5/31/20.
+//  Copyright © 2020 TejasMehta. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ComparisonTests : XCTestCase
+
+@end
+
+@implementation ComparisonTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
